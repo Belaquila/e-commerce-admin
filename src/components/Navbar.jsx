880646ce6a5d715c34be98dd589fc6b851a07af9
ab.belabdia@gmail.com <ahmed@MacBook-Pro-de-Ahmed.local>
2024-10-30 19:34:09 +0100

    add logo and css enhancement

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
-import logo from "../assets/react.svg";
+import logo from "/src/assets/logo_Ecommerce.svg";
 
 function Navbar() {
   return (
-    <div className="bg-blue-600 text-white shadow-md fixed top-0 left-0 w-full z-50 flex columns-1">
-      <img className="h-10 w-auto border-solid border border-white rounded-3xl p-1" src={logo} />
-      <p className="flex columns-4">e-commerce admin</p>
+
+    <div className="navbar">
+      <img className="h-20 w-auto border-solid rounded-3xl p-1" src={logo} />
+      <p className="flex columns-4 bg-center centered-text"> e-commerce manager </p>
     </div>
   );
 }
